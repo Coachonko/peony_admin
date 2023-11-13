b6e6cbf3fb4d79349195763dceb51b3ac6e63e22
Coachonko <info@coachonko.com>
2023-11-13 21:32:31 +0100

    implement update handler

--- src/components/Settings/Store/Store.js
+++ src/components/Settings/Store/Store.js
@@ -1,4 +1,4 @@
-import { Component } from 'inferno'
+import { Component, linkEvent } from 'inferno'
 
 import { config } from '../../../../config'
 import { appendToken, getToken } from '../../../utils/auth'
@@ -13,9 +13,11 @@ export default class Store extends Component {
 
     this.state = {
       storeData: null,
+      sortedMetadata: null,
+      isSubmitting: false,
+      hasUpdated: null,
       errorStatus: null,
-      errorBody: null,
-      sortedMetadata: null
+      errorBody: null
     }
 
     this.updateLastError = this.updateLastError.bind(this)
@@ -33,6 +35,12 @@ export default class Store extends Component {
   async componentDidMount () {
     this.gettingStoreData = makeCancelable(this.getStoreData())
     await this.resolveGettingStoreData()
+  }
+
+  componentDidUpdate () {
+    if (this.state.hasUpdated === true) {
+      this.setState({ hasUpdated: false })
+    }
   }
 
   componentWillUnmount () {
@@ -98,84 +106,216 @@ export default class Store extends Component {
 
   render (props) {
     if (this.state.storeData) {
+      const storeData = this.state.storeData
+
       return (
         <div>
           <h2>Store</h2>
-          <Table data={this.state.storeData}>
-            <tr>
-              <th scope='row'>metadata</th>
-              <td data-cell='metadata'>
-                <Metadata
-                  lastError={this.state.lastError}
-                  updateLastError={this.updateLastError}
-                  sortedMetadata={this.state.sortedMetadata}
-                  updateSortedMetadata={this.updateSortedMetadata}
-                />
-              </td>
-            </tr>
-          </Table>
+          <div>
+            <button
+              type='button'
+              onClick={linkEvent(this, handleUpdate)}
+            >
+              save
+            </button>
+          </div>
+          <table>
+            <caption>
+              Store settings
+            </caption>
+
+            <tbody>
+              <tr>
+                <th scope='row'>ID</th>
+                <td data-cell='id'>{storeData.id}</td>
+              </tr>
+              <tr>
+                <th scope='row'>Creation date</th>
+                <td data-cell='createdAt'>{storeData.createdAt}</td>
+              </tr>
+              <tr>
+                <th scope='row'>Last updated</th>
+                <td data-cell='updatedAt'>{storeData.updatedAt}</td>
+              </tr>
+              <tr>
+                <th scope='row'>Name</th>
+                <td data-cell='name'>
+                  <input
+                    name='name'
+                    type='text'
+                    spellCheck='false'
+                    autoComplete='off'
+                    value={this.state.storeData.name}
+                    onInput={linkEvent(this, handleInputChange)}
+                    disabled={this.state.isSubmitting}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th scope='row'>Default locale</th>
+                <td data-cell='defaultLocaleCode'>
+                  {storeData.defaultLocaleCode}
+                  {/* TODO should be a select */}
+                </td>
+              </tr>
+              <tr>
+                <th scope='row'>Default currency</th>
+                <td data-cell='defaultCurrencyCode'>
+                  {storeData.defaultCurrencyCode}
+                  {/* TODO should be a select */}
+                </td>
+              </tr>
+              {/* <tr>
+                <th scope='row'>Swap link template</th>
+                <td data-cell='swapLinkTemplate'>{storeData.swapLinkTemplate}</td>
+              </tr>
+              <tr>
+                <th scope='row'>Payment link template</th>
+                <td data-cell='paymentLinkTemplate'>{storeData.paymentLinkTemplate}</td>
+              </tr> */}
+              <tr>
+                <th scope='row'>Invite link template</th>
+                <td data-cell='inviteLinkTemplate'>
+                  {storeData.inviteLinkTemplate}
+                  {/* TODO don't know yet */}
+                </td>
+              </tr>
+              <tr>
+                <th scope='row'>Default stock location</th>
+                <td data-cell='defaultStockLocationId'>
+                  {storeData.defaultStockLocationId}
+                  {/* TODO don't know yet */}
+                </td>
+              </tr>
+              <tr>
+                <th scope='row'>Default sales channel</th>
+                <td data-cell='defaultSalesChannelId'>
+                  {storeData.defaultSalesChannelId}
+                  {/* TODO don't know yet */}
+                </td>
+              </tr>
+
+              <tr>
+                <th scope='row'>Metadata</th>
+                <td data-cell='metadata'>
+                  <Metadata
+                    lastError={this.state.lastError}
+                    updateLastError={this.updateLastError}
+                    sortedMetadata={this.state.sortedMetadata}
+                    updateSortedMetadata={this.updateSortedMetadata}
+                  />
+                </td>
+              </tr>
+
+            </tbody>
+
+          </table>
+
         </div>
       )
     }
   }
 }
 
-function Table ({ data, children }) {
-  return (
-    <table>
-      <caption>
-        Store settings
-      </caption>
+async function handleUpdate (instance) {
+  instance.setState({ isSubmitting: true })
 
-      <tbody>
-        <tr>
-          <th scope='row'>id</th>
-          <td data-cell='id'>{data.id}</td>
-        </tr>
-        <tr>
-          <th scope='row'>createdAt</th>
-          <td data-cell='createdAt'>{data.createdAt}</td>
-        </tr>
-        <tr>
-          <th scope='row'>updatedAt</th>
-          <td data-cell='updatedAt'>{data.updatedAt}</td>
-        </tr>
-        <tr>
-          <th scope='row'>name</th>
-          <td data-cell='name'>{data.name}</td>
-        </tr>
-        <tr>
-          <th scope='row'>defaultLocaleCode</th>
-          <td data-cell='defaultLocaleCode'>{data.defaultLocaleCode}</td>
-        </tr>
-        <tr>
-          <th scope='row'>defaultCurrencyCode</th>
-          <td data-cell='defaultCurrencyCode'>{data.defaultCurrencyCode}</td>
-        </tr>
-        <tr>
-          <th scope='row'>swapLinkTemplate</th>
-          <td data-cell='swapLinkTemplate'>{data.swapLinkTemplate}</td>
-        </tr>
-        <tr>
-          <th scope='row'>paymentLinkTemplate</th>
-          <td data-cell='paymentLinkTemplate'>{data.paymentLinkTemplate}</td>
-        </tr>
-        <tr>
-          <th scope='row'>inviteLinkTemplate</th>
-          <td data-cell='inviteLinkTemplate'>{data.inviteLinkTemplate}</td>
-        </tr>
-        <tr>
-          <th scope='row'>defaultStockLocationId</th>
-          <td data-cell='defaultStockLocationId'>{data.defaultStockLocationId}</td>
-        </tr>
-        <tr>
-          <th scope='row'>defaultSalesChannelId</th>
-          <td data-cell='defaultSalesChannelId'>{data.defaultSalesChannelId}</td>
-        </tr>
-        {children}
+  if (!instance.state.storeData.name) {
+    const newError = new Error('name is required')
+    console.error(newError)
+    instance.setState({
+      isSubmitting: false,
+      lastError: newError
+    })
+    return
+  }
+  if (!instance.state.storeData.defaultLocaleCode) {
+    const newError = new Error('defaultLocaleCode is required')
+    console.error(newError)
+    instance.setState({
+      isSubmitting: false,
+      lastError: newError
+    })
+    return
+  }
+  if (!instance.state.storeData.defaultCurrencyCode) {
+    const newError = new Error('defaultCurrencyCode is required')
+    console.error(newError)
+    instance.setState({
+      isSubmitting: false,
+      lastError: newError
+    })
+    return
+  }
 
-      </tbody>
+  const {
+    name,
+    defaultLocaleCode,
+    defaultCurrencyCode,
+    inviteLinkTemplate,
+    defaultStockLocationId,
+    defaultSalesChannelId
+  } = instance.state.postData
 
-    </table>
-  )
+  const metadataObject = instance.state.sortedMetadata.reduce((acc, curr) => ({ ...acc, ...curr }), {})
+
+  const storeDataToSend = {
+    name,
+    defaultLocaleCode,
+    defaultCurrencyCode,
+    inviteLinkTemplate,
+    defaultStockLocationId,
+    defaultSalesChannelId,
+    metadata: metadataObject
+  }
+
+  const data = await updatePostData(storeDataToSend, instance.state.storeData.id)
+
+  if (data instanceof Error) {
+    console.error(data)
+    instance.setState({
+      lastError: data,
+      isSubmitting: false
+    })
+  } else {
+    if (isPeonyError(data)) {
+      instance.setState({
+        peonyError: data,
+        isSubmitting: false
+      })
+    } else {
+      instance.setState({
+        hasUpdated: true, // TODO use this to show user a save was successful
+        isSubmitting: false
+      })
+    }
+  }
+}
+
+async function updatePostData (storeDataToSend, id) {
+  const token = getToken()
+  const requestHeaders = new Headers()
+  requestHeaders.append('Content-Type', 'application/json')
+  appendToken(token, requestHeaders)
+
+  try {
+    const response = await fetch(`${config.PEONY_ADMIN_API}/store/${id}`, {
+      method: 'POST',
+      headers: requestHeaders,
+      body: JSON.stringify(storeDataToSend)
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return error
+  }
+}
+
+function handleInputChange (instance, event) {
+  instance.setState({
+    storeData: {
+      ...instance.state.storeData,
+      [event.target.name]: event.target.value
+    }
+  })
 }
