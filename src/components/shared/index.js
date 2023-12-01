5d725ad20148ea160cc3bb277354d1c3a135bee3
Coachonko <info@coachonko.com>
2023-12-01 15:24:07 +0100

    Remove useless Select component

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,6 +1,5 @@
 import Alerts from './Alerts'
 import Metadata from './Metadata'
 import Nav from './Nav'
-import Select from './Select'
 
-export { Alerts, Metadata, Nav, Select }
+export { Alerts, Metadata, Nav }
